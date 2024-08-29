5ee6cbb5650e43b06449a20da6769eadb3dd36ec
NHBKhang <nhbkhang12@gmail.com>
2024-08-29 20:39:54 +0700

    Merge

--- FE/app/src/reducers/UseReducer.js
+++ FE/app/src/reducers/UseReducer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const UseReducer = (user, action) => {
     switch (action.type) {
@@ -18,25 +17,4 @@ const UseReducer = (user, action) => {
     }
 }
 
-=======
-
-const UseReducer = (user, action) => {
-    switch (action.type) {
-        case "login": {
-            localStorage.setItem('user', JSON.stringify(action.payload));
-            return action.payload
-        }
-        case "logout": {
-            localStorage.removeItem('user');
-            return null;
-        }
-        case "upstore":
-            return JSON.parse(localStorage.getItem('user'))
-
-        default: 
-            return user;
-    }
-}
-
->>>>>>> 4b7ae76bcaddb780f7cbf1f931b9e71a240a5481
 export default UseReducer
