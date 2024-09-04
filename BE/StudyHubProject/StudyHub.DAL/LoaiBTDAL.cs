f59184a7755350f21e7551abf9bd2e48f49e0271
NHBKhang <nhbkhang12@gmail.com>
2024-09-04 18:44:56 +0700

    Them bai tap

--- BE/StudyHubProject/StudyHub.DAL/LoaiBTDAL.cs
+++ BE/StudyHubProject/StudyHub.DAL/LoaiBTDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudyHub.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace StudyHub.DAL
 {
-    internal class LoaiBTDAL
+    public class LoaiBTDAL
     {
+        private HeThongQuanLyHocTapContext _context = new HeThongQuanLyHocTapContext();
+
+        public List<LoaiBaiTap> GetAll()
+        {
+            return _context.LoaiBaiTaps.ToList();
+        }
     }
 }
