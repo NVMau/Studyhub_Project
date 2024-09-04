c5acd68b88e5972299bcffdc96c9c363a05e4503
Cao-nguyen <caonguyentran1011@gmail.com>
2024-09-04 10:49:52 +0700

    commit lesson in course

--- BE/StudyHubProject/AdminStudyHub/Controllers/HocKysController.cs
+++ BE/StudyHubProject/AdminStudyHub/Controllers/HocKysController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using StudyHub.BLL;
 using StudyHub.DAL.Models;
 
