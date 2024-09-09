cd60a7aa5cb7208f603340161624683c0627f8c2
nmau4 <nmau4@BEOSBUJ>
2024-09-09 22:13:13 +0700

    Add Thong ke in Admin Site

---
+++ BE/StudyHubProject/AdminStudyHub/Controllers/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudyHub.BLL;
+using StudyHub.BLL.StudyHub.BLL;
+using AdminStudyHub.Models;
+
+namespace AdminStudyHub.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly UserBLL _userBLL;
+        private readonly MonHocBLL _monHocBLL;
+        private readonly KhoaHocBLL _khoaHocBLL;
+
+        public StatisticsController()
+        {
+            _userBLL = new UserBLL();
+            _monHocBLL = new MonHocBLL();
+            _khoaHocBLL = new KhoaHocBLL();
+        }
+
+        public IActionResult Index()
+        {
+            var totalUsers = _userBLL.GetAllUsers().Count;
+            var totalMonHocs = _monHocBLL.getMonhocs().Count;
+            var totalKhoaHocs = _khoaHocBLL.GetAllKhoaHoc().Count;
+
+            var model = new StatisticsViewModel
+            {
+                TotalUsers = totalUsers,
+                TotalMonHocs = totalMonHocs,
+                TotalKhoaHocs = totalKhoaHocs
+            };
+
+            return View(model);
+        }
+    }
+}
--- BE/StudyHubProject/StudyHubBLL/obj/Debug/net8.0/StudyHub.BLL.AssemblyInfo.cs
+++ BE/StudyHubProject/StudyHubBLL/obj/Debug/net8.0/StudyHub.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyHub.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8620b444bbba810888035ac438ba7b0ee2db4580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ab140b0e0a0a80fb317b1ba04f4949f0da3a1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyHub.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyHub.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
